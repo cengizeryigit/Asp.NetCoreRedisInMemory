57a382350229dda354f4c2f5d0e47f55a9fb0fb6
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-13 23:25:07 +0300

    AddMemoryCache service and IMemoryCache Interface

---
+++ Asp.Net Core + In-Memory/InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
