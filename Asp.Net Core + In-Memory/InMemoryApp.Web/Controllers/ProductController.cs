dc780e28254ce08eef8659de87645993b6dcf8f2
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-18 18:11:10 +0300

    RegisterPostEvictionCallback

--- Asp.Net Core + In-Memory/InMemoryApp.Web/Controllers/ProductController.cs
+++ Asp.Net Core + In-Memory/InMemoryApp.Web/Controllers/ProductController.cs
@@ -17,7 +17,10 @@ namespace InMemoryApp.Web.Controllers
 			options.AbsoluteExpiration = DateTime.Now.AddMinutes(5);
 			options.SlidingExpiration = TimeSpan.FromMinutes(1);
 			options.Priority = CacheItemPriority.High;
-
+			options.RegisterPostEvictionCallback((key, value, reason, state) =>
+			{
+				_memoryCache.Set("callback", $"{key}-->{value} => sebep: {reason}");
+			});
 			_memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
 			return View();
@@ -26,7 +29,8 @@ namespace InMemoryApp.Web.Controllers
 		public IActionResult Show()
 		{
 			_memoryCache.TryGetValue("zaman", out string zamancache);
-
+			_memoryCache.TryGetValue("callback", out string callback);
+			ViewBag.callback = callback;
 			ViewBag.zaman = zamancache;
 
 			return View();
