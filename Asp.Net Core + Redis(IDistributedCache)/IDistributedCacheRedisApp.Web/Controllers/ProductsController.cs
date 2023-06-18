d2b9900e143a5f0de425dad54ca50b2e5741621e
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-18 22:29:33 +0300

    AddStackExchangeRedisCache() service ve IDistributedCache interface

---
+++ Asp.Net Core + Redis(IDistributedCache)/IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
