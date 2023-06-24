69f4f8cf93d4a2e9bdfaf7c3cf13b80a873fee73
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-24 15:18:11 +0300

    AppDbContext

---
+++ Asp.Net Core 7 + Redis Example API/RedisExampleAPP/RedisExampleAPP.API/Models/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.API.Models
+{
+	public class AppDbContext:DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(
+				new Product() { Id=1,Name="Kalem 1"},
+				new Product() { Id = 2, Name = "Kalem 2" },
+				new Product() { Id = 3, Name = "Kalem 3" }
+				);
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
