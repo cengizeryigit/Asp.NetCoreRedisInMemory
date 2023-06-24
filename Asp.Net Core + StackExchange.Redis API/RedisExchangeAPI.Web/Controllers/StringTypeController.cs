3069dc62e723a5147577df0172f0ffd6b411a1b8
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-24 10:51:12 +0300

    StackExchange.Redis API / Redis String

---
+++ Asp.Net Core + StackExchange.Redis API/RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+	public class StringTypeController : Controller
+	{
+		private readonly RedisService _redisService;
+		private readonly IDatabase db;
+        public StringTypeController(RedisService redisService )
+        {
+			_redisService = redisService;
+			db = _redisService.GetDb(0);
+		}
+        public IActionResult Index()
+		{
+
+			db.StringSet("name", "Cengiz Eryiğit");
+			db.StringSet("ziyaretci", 100);
+
+			return View();
+		}
+
+		public IActionResult Show()
+		{
+			var value = db.StringGet("name");
+			db.StringIncrement("ziyaretci", 1);
+			var count = db.StringDecrementAsync("ziyaretci", 1).Result;
+			db.StringDecrementAsync("ziyaretci", 10).Wait();
+			var valueRange = db.StringGetRange("name", 0, 3);
+			var valueLenght = db.StringLength("name");
+			if(value.HasValue)
+			{
+				ViewBag.value = value.ToString();
+			}
+
+			return View();
+		}
+	}
+}
