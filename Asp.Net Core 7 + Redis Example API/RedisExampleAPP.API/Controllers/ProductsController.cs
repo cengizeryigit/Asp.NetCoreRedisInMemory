51c0aab3d4addb287eb404d2ef1286bcbea7426f
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-07-05 20:32:26 +0300

    Integrating the cache structure 3

--- Asp.Net Core 7 + Redis Example API/RedisExampleAPP.API/Controllers/ProductsController.cs
+++ Asp.Net Core 7 + Redis Example API/RedisExampleAPP.API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RedisExampleApp.API.Models;
 using RedisExampleApp.API.Repositories;
+using RedisExampleApp.API.Services;
 using RedisExampleApp.Cache;
 using StackExchange.Redis;
 
@@ -11,29 +12,29 @@ namespace RedisExampleApp.API.Controllers
 	[ApiController]
 	public class ProductsController : ControllerBase
 	{
-		private readonly IProductRepository _productRepository;
+		private readonly IProductService _productService;
 		private readonly IDatabase _database;
 		
-		public ProductsController(IProductRepository productRepository)
+		public ProductsController(IProductService productService)
 		{
-			_productRepository = productRepository;
+			_productService = productService;
 
 		}
 		[HttpGet]
 		public async Task<IActionResult> GetAll()
 		{
-			return Ok(await _productRepository.GetAsync());
+			return Ok(await _productService.GetAsync());
 		}
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetById(int id)
 		{
-			return Ok(await _productRepository.GetByIdAsync(id));
+			return Ok(await _productService.GetByIdAsync(id));
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> Create(Product product)
 		{
-			return Created(string.Empty,await _productRepository.CreateAsync(product));
+			return Created(string.Empty,await _productService.CreateAsync(product));
 		}
 	}
 }
